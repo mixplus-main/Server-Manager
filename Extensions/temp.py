33a32a476d59b8e69d8a8f2426f53c95d3408562
mixplus <wuyouqitian@gmail.com>
2026-02-05 15:56:36 +0900

    Update temp.py

--- Extensions/temp.py
+++ Extensions/temp.py
@@ -1,1 +1,45 @@
+import tkinter as tk
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from ServerManager_class import AppContext, MAIN, GUI, Config, TAB
 
+def apply(ctx: "AppContext"):
+    t = Temp(ctx)
+
+class Temp:
+    def __init__(self, ctx: "AppContext"):
+        self.main: "MAIN" = ctx.c2
+        self.gui: "GUI" = ctx.c4
+        self.CFG: "Config" = ctx.c1
+        self.tab: "TAB" = ctx.c3
+        
+        Temp_frame = tk.Button(self.main.win, text="Temp", command=self.Temp_tab, bg=self.CFG.btn_color, fg=self.CFG.fg)
+        Temp_frame.pack()
+    
+    def Temp_tab(self):
+        frame_temp = tk.Frame(self.main.win, bg=self.CFG.bg); frame_temp.place(x=0, y=20, width=910, height=490)
+        temp_label = tk.Label(frame_temp, text="Temp", bg=self.CFG.bg, fg=self.CFG.fg, font=("arial", 12)); temp_label.pack()
+        
+        log_temp = tk.Text(
+            frame_temp,
+            bg=self.CFG.bg, fg=self.CFG.fg,
+            font=("arial", 12), state="disabled"); log_temp.place(x=0, y=30, width=900, height=405)
+        
+        scroll = tk.Scrollbar(frame_temp, command=log_temp.yview); scroll.place(x=890, y=30, height=400)
+        log_temp.config(yscrollcommand=scroll.set)
+        
+        Entry_temp = tk.Entry(frame_temp, width=90, font=("arial", 15), bg=self.CFG.btn_color, fg=self.CFG.fg); Entry_temp.place(y=440, x=0)
+        
+        #setting
+        self.tab.frames.append(frame_temp)
+        self.tab.labels.append(temp_label)
+        self.tab.text.append(log_temp)
+        self.tab.scrollbars.append(scroll)
+        self.tab.entry.append(Entry_temp)
+        
+        Entry_temp.bind("<Return>", self.main.on_enter)
+        Entry_temp.bind("<Up>", self.main.clip_up)
+        Entry_temp.bind("<Down>", self.main.clip_down_)
+        self.main.log_boxes['temp'] = log_temp
+        self.main.add_log("Temp", "green", "temp")
+        return frame_temp, temp_label
