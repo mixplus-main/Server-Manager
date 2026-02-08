8380fccfc60649d2f092e9fb7d526c8217895a58
mixplus <wuyouqitian@gmail.com>
2026-02-08 11:46:27 +0900

    Refactor button and frame creation for readability

--- Extensions/temp.py
+++ Extensions/temp.py
@@ -14,21 +14,30 @@ class Temp:
         self.CFG: "Config" = ctx.cfg
         self.tab: "TAB" = ctx.tab
         
-        Temp_frame = tk.Button(self.main.win, text="Temp", command=self.Temp_tab, bg=self.CFG.btn_color, fg=self.CFG.fg)
+        Temp_frame = tk.Button(
+            self.main.win, text="Temp", command=self.Temp_tab,
+            bg=self.CFG.btn_color, fg=self.CFG.fg)
         Temp_frame.pack()
     
     def Temp_tab(self):
         if "frame_temp" not in self.tab.frames or not self.tab.frames["frame_temp"].winfo_exists():
-            frame_temp = tk.Frame(self.main.win, bg=self.CFG.bg); frame_temp.place(x=0, y=20, width=910, height=490)
+            frame_temp = tk.Frame(
+                self.main.win, bg=self.CFG.bg)
+            frame_temp.place(x=0, y=20, width=910, height=490)
+            
             self.tab.frames.append(frame_temp)
             self.tab.frame_dict["temp"] = frame_temp
         
-        temp_label = tk.Label(frame_temp, text="Temp", bg=self.CFG.bg, fg=self.CFG.fg, font=("arial", 12)); temp_label.pack()
+        temp_label = tk.Label(
+            frame_temp, text="Temp", bg=self.CFG.bg,
+            fg=self.CFG.fg, font=("arial", 12))
+        temp_label.pack()
         
         log_temp = tk.Text(
             frame_temp,
             bg=self.CFG.bg, fg=self.CFG.fg,
-            font=("arial", 12), state="disabled"); log_temp.place(x=0, y=30, width=900, height=405)
+            font=("arial", 12), state="disabled")
+        log_temp.place(x=0, y=30, width=900, height=405)
         
         scroll = tk.Scrollbar(frame_temp, command=log_temp.yview); scroll.place(x=890, y=30, height=400)
         log_temp.config(yscrollcommand=scroll.set)
